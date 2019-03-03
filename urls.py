2241350303004fdf46bcda95325a5a2e3c7df7fa
Anirudh Goel <ani050599@gmail.com>
2019-03-03 18:45:03 +0530

    Gravatar added

---
+++ urls.py
@@ -0,0 +1,16 @@
+from django.urls import path,include
+from .views import SignUpView,HomePageView,SignInView,sign_out_view,UserPostListView,UserProfileView,UserListView,user_self_profile,upvote,downvote
+
+app_name = 'account'
+urlpatterns = [
+    path('sign_up/',SignUpView.as_view(),name='sign_up'),
+    path('',HomePageView.as_view(),name='home_page'),
+    path('sign_in/',SignInView.as_view(),name='sign_in'),
+    path('sign_out/',sign_out_view,name='sign_out'),
+    path('user/',UserPostListView.as_view(),name='user_posts'),
+    path('user/<int:pk>/',UserProfileView.as_view(),name='user_profile'),
+    path('users_all/',UserListView.as_view(),name='user_list'),
+    path('user/self/',user_self_profile,name='user_self_profile'),
+    path('user/<int:pk>/upvote',upvote,name='post_upvote'),
+    path('user/<int:pk>/downvote',downvote,name='post_downvote'),
+]
